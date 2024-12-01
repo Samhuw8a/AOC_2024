ca56d9d28da9712461f318ba839c15c2e468a018
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-12-01 21:19:38 +0100

    Refactor

--- day_01.py
+++ day_01.py
@@ -4,28 +4,30 @@ from collections import Counter
 input_1 = read_input_line(1, sep="\n")
 # input_1 = read_input_line("test_01", sep="\n")
 
-l1:list = []
-l2:list = []
+l1: list = []
+l2: list = []
 for i in input_1:
     line = list(integers(i))
     l1.append(line[0])
     l2.append(line[1])
-
 l1.sort()
 l2.sort()
 
-tot:int = 0
-for i,j in zip(l1,l2):
-    tot += abs(i-j)
-# print(l1)
-# print(l2)
-print(tot)
+
+def part_1() -> int:
+    tot: int = 0
+    for i, j in zip(l1, l2):
+        tot += abs(i-j)
+    return tot
 
 
-c2 = Counter(l2)
+def part_2() -> int:
+    c2 = Counter(l2)
+    tot: int = 0
+    for i in l1:
+        tot += i*c2[i]  # type:ignore
+    return tot
 
-part_2:int =0
-for i in l1:
-    part_2 += i*c2[i] #type:ignore
 
-print(part_2)
+print(part_1())
+print(part_2())
