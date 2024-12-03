ddd1a23ec6eea029712e0d0fa086d9dfc2b46e51
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-12-03 08:36:58 +0100

    03 Remaster

--- day_03.py
+++ day_03.py
@@ -1,34 +1,36 @@
 from utils.helpers import *
 import re
 
-input = read_input_line(3,sep="ökasjdföalskdhg")
-input = read_input_line("test_03",sep="adsflkjasdfölkjasd")
+# input = read_input_line(3, sep="ökasjdföalskdhg")
+input = read_input_line("test_03", sep="adsflkjasdfölkjasd")
 
-pattern = re.compile("mul\([0-9]{1,3},[0-9]{1,3}\)")
-res = pattern.findall(input[0])
-# print(res)
-
-tot = 0
-for el in res:
-    nums = list(integers(el))
-    # print(nums)
-    tot += nums[0]*nums[1]
-print(tot)
+pattern1 = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)")
+pattern2 = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)")
 
 
-pattern2 = re.compile("mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)")
-matchs = pattern2.findall(input[0])
-
-tot=0
-enabled=True
-for el in matchs:
-    if el =="do()":
-        enabled=True
-    elif el =="don't()":
-        enabled = False
-    elif enabled:
+def part_1() -> int:
+    tot = 0
+    found = pattern1.findall(input[0])
+    for el in found:
         nums = list(integers(el))
         tot += nums[0]*nums[1]
+    return tot
 
-print(tot)
 
+def part_2() -> int:
+    found = pattern2.findall(input[0])
+    tot = 0
+    enabled = True
+    for el in found:
+        if el == "do()":
+            enabled = True
+        elif el == "don't()":
+            enabled = False
+        elif enabled:
+            nums = list(integers(el))
+            tot += nums[0]*nums[1]
+    return tot
+
+
+print(part_1())
+print(part_2())
